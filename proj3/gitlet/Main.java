4f1c1309f660fa0d7c3d3cbed5fed75bbec85687
Darren Wang <hdwang@berkeley.edu>
2022-04-29 22:51:06 -0700

    Update for testing.

--- proj3/gitlet/Main.java
+++ proj3/gitlet/Main.java
@@ -1,5 +1,6 @@
 package gitlet;
 
+import static gitlet.Command.init;
 import static gitlet.Files.*;
 import static gitlet.Utils.*;
 import static gitlet.Command.*;
@@ -15,30 +16,15 @@ public class Main {
         if (args.length == 0) {
             throw error("No command detected.");
         }
-
         String cases = args[0];
-
         if (cases.equals("init")) {
             validateArg(args, 1);
-            if (CWD.exists()) {
-                System.out.println("A Gitlet version-control system" +
-                        " already exists in the current directory.");
-                return;
-            }
-            init();
+            initMain();
         } else if (cases.equals("add")) {
             validateArg(args, 2);
-            if (!join(args[1]).exists()) {
-                System.out.println("File does not exist.");
-                return;
-            }
-            add(args[1]);
+            addMain(args);
         } else if (cases.equals("commit")) {
-            if (args.length != 2) {
-                System.out.println("Please enter a commit message.");
-                return;
-            }
-            commit(args[1]);
+            commitMain(args);
         } else if (cases.equals("rm")) {
             validateArg(args, 2);
             rm(args[1]);
@@ -55,16 +41,7 @@ public class Main {
             validateArg(args, 1);
             status();
         } else if (cases.equals("checkout")) {
-            if (args.length == 4) {
-                checkoutPastFile(args[1], args[3]);
-            } else if (args.length == 3) {
-                checkoutHeadFile(args[2]);
-            } else if (args.length == 2) {
-                checkoutBranch(args[1]);
-            } else {
-                throw error("Not a valid input for ",
-                        "checkout");
-            }
+            checkoutMain(args);
         } else if (cases.equals("branch")) {
             validateArg(args, 2);
             branch(args[1]);
@@ -78,7 +55,46 @@ public class Main {
             validateArg(args, 2);
             merge(args[1]);
         } else {
-            throw error("Invalid command.");
+            System.out.println("Invalid command.");
+            return;
+        }
+    }
+
+    static void initMain() {
+        if (CWD.exists()) {
+            System.out.println("A Gitlet version-control system" +
+                    " already exists in the current directory.");
+            return;
+        }
+        init();
+    }
+
+    static void addMain(String... args) {
+        if (!join(args[1]).exists()) {
+            System.out.println("File does not exist.");
+            return;
+        }
+        add(args[1]);
+    }
+
+    static void commitMain(String... args) {
+        if (args.length != 2) {
+            System.out.println("Please enter a commit message.");
+            return;
+        }
+        commit(args[1]);
+    }
+
+    static void checkoutMain(String... args) {
+        if (args.length == 4) {
+            checkoutPastFile(args[1], args[3]);
+        } else if (args.length == 3) {
+            checkoutHeadFile(args[2]);
+        } else if (args.length == 2) {
+            checkoutBranch(args[1]);
+        } else {
+            System.out.println("Not a valid input for checkout.");
+            return;
         }
     }
 
